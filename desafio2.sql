c0a25a9c374c2aa5033baaa569664c52d5f876bb
Felipe Martins <mfelipeluiz99@gmail.com>
2022-01-10 19:42:46 -0300

    Desafio2.sql FIX

--- desafio2.sql
+++ desafio2.sql
@@ -1,10 +1,10 @@
 SELECT
-COUNT(DISTINCT cancoes.cancao_id) AS cancoes,
-COUNT(DISTINCT artista.artista_id) AS artistas,
-COUNT(DISTINCT albuns.album_id) AS albuns
+COUNT(DISTINCT `cancoes.cancao_id`) AS `cancoes`,
+COUNT(DISTINCT `artista.artista_id`) AS `artistas`,
+COUNT(DISTINCT `albuns.album_id`) AS `albuns`
 
-FROM SpotifyClone.cancoes
-    INNER JOIN SpotifyClone.artista AS artista
-    INNER JOIN SpotifyClone.albuns AS albuns;
+FROM `SpotifyClone.cancoes`
+    INNER JOIN `SpotifyClone.artista` AS `artista`
+    INNER JOIN `SpotifyClone.albuns` AS `albuns`;
     
 --https://www.w3resource.com/sql/aggregate-functions/count-with-distinct.php
