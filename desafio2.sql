3425c655a345305545bfc2ef7609634e0908c210
Felipe Martins <mfelipeluiz99@gmail.com>
2022-01-10 18:53:44 -0300

    Desafio2.sql

---
+++ desafio2.sql
@@ -0,0 +1,10 @@
+SELECT
+COUNT(DISTINCT cancoes.cancao_id) AS cancoes,
+COUNT(DISTINCT artista.artista_id) AS artistas,
+COUNT(DISTINCT albuns.album_id) AS albuns
+
+FROM SPOTIFY_CLONE
+    INNER JOIN SpotifyClone.artista AS artista
+    INNER JOIN SpotifyClone.albuns AS albuns;
+    
+--https://www.w3resource.com/sql/aggregate-functions/count-with-distinct.php
