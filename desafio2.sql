7db1a9b2c79c23ac129e7c6cfecc2f06809b6c8f
Felipe Martins <mfelipeluiz99@gmail.com>
2022-01-11 11:53:21 -0300

    desafio2.sql

--- desafio2.sql
+++ desafio2.sql
@@ -3,7 +3,7 @@ SELECT
     COUNT(DISTINCT `artista.artista_id`) AS `artistas`,
     COUNT(DISTINCT `albuns.album_id`) AS `albuns`
 
-FROM `SpotifyClone.cancoes`
+FROM `SpotifyClone.cancoes` as `cancoes`
     INNER JOIN `SpotifyClone.artista` AS `artista`
     INNER JOIN `SpotifyClone.albuns` AS `albuns`;
     
