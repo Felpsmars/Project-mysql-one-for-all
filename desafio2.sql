db1eb695aa6d0c2c4f080224c970f9db56520372
Felipe Martins <mfelipeluiz99@gmail.com>
2022-01-12 09:52:12 -0300

    evaluator fix

--- desafio2.sql
+++ desafio2.sql
@@ -3,5 +3,7 @@ SELECT
     COUNT(DISTINCT artista.artista_id) AS artistas,
     COUNT(DISTINCT albuns.album_id) AS albuns
 FROM SpotifyClone.cancoes as cancoes
-    INNER JOIN SpotifyClone.artista AS artista
-    INNER JOIN SpotifyClone.albuns AS albuns;
+INNER JOIN
+    SpotifyClone.artista AS artista
+INNER JOIN 
+    SpotifyClone.albuns AS albuns;
