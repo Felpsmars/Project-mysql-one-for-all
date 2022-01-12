bf876817b233ef92a2d7a493d277c18cf8af858a
Felipe Martins <mfelipeluiz99@gmail.com>
2022-01-12 10:04:43 -0300

    refact desfio6.sql

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 SELECT 
   MIN(planos.valor_plano) AS 'faturamento_minimo',
   MAX(planos.valor_plano) AS 'faturamento_maximo',
-  CAST((AVG(plans.valor_plano)) AS DECIMAL(4,2)) AS 'faturamento_medio',
-  CAST((SUM(plans.valor_plano)) AS DECIMAL(4,2)) AS 'faturamento_total'
+  CAST((AVG(planos.valor_plano)) AS DECIMAL(4,2)) AS 'faturamento_medio',
+  CAST((SUM(planos.valor_plano)) AS DECIMAL(4,2)) AS 'faturamento_total'
 FROM
   SpotifyClone.planos AS planos,
   SpotifyClone.usuario AS usuario
